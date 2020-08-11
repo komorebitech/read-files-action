595b4919950a4698f21f805ffa031eb83b8e7af9
Sudhanshu Mishra <mrsud94@gmail.com>
2020-08-11 18:09:45 +0530

    Add logging

--- dist/index.js
+++ dist/index.js
@@ -61,12 +61,15 @@ const { promises: fs } = __webpack_require__(747);
 
 const main = async () => {
   const filePaths = core.getInput('files').split(" ");
+  console.log(filePaths);
   const output = "";
   for (var filePath in filePaths){
+    console.log(filePath);
     if(filePaths.includes("migrations")){
       output += `==========================${filePath}==========================\n\n`;
       const content = await fs.readFile(filePath, 'utf8');
       output += content + "\n\n";
+      console.log(content);
     }
   }
   core.setOutput('content', output);
--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const { promises: fs } = require('fs');
 
 const main = async () => {
   const filePaths = core.getInput('files').split(" ");
+  console.log(filePaths);
   const output = "";
   for (var filePath in filePaths){
+    console.log(filePath);
     if(filePaths.includes("migrations")){
       output += `==========================${filePath}==========================\n\n`;
       const content = await fs.readFile(filePath, 'utf8');
       output += content + "\n\n";
+      console.log(content);
     }
   }
   core.setOutput('content', output);
