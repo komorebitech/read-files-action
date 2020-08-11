117bfa5daa34619db75f7cc32793ab692941b42c
Smit Patel <thebinarymutant@users.noreply.github.com>
2020-08-11 19:10:44 +0530

    Match files using a regEx pattern

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 'use strict'
 
-const core = require('@actions/core');
-const { promises: fs } = require('fs');
+const core = require('@actions/core')
+const { promises: fs } = require('fs')
 
 const main = async () => {
-  const filePaths = JSON.parse(core.getInput('files'));
-  console.log(filePaths);
-  var output = "";
+  const filePaths = JSON.parse(core.getInput('files'))
+  const regularExpression = JSON.parse(core.getInput('pattern'))
+  
+  const pattern = new RegExp(regularExpression);
 
-  filePaths.forEach(async(filePath) => {
+  console.log(filePaths, pattern)
+  var output = ''
 
-    if(filePath.includes("migrations")){
-      output += `========================== ${filePath} ==========================\n\n`;
-      const content = await fs.readFile(filePath, 'utf8');
-      output += content + "\n\n";
-
+  filePaths.forEach(async filePath => {
+    if (pattern.test(filePath)) {
+      output += `========================== ${filePath} ==========================\n\n`
+      const content = await fs.readFile(filePath, 'utf8')
+      output += content + '\n\n'
     }
-
-  });
-  core.setOutput('content', output);
+  })
+  core.setOutput('content', output)
 }
 
-main().catch(err => core.setFailed(err.message));
+main().catch(err => core.setFailed(err.message))
